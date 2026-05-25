183cb877bb757f2c314b800d587ddd66ad628916
PedroLearning <pedroescolagospel@gmail.com>
2026-05-25 13:26:13 +0100

    Question on it!

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request 
+from dbqueries import get_questions_en
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    questions = get_questions_en()
     if request.method == 'POST':
         pass
     if request.method == 'GET': 
-        return render_template('home.html')
-    
-    questions = get_questions_by_category(categoryID)
-    return render_template('home.html', questions=questions)
-    
+        return render_template('home.html', questions=questions)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dbqueries.py
+++ dbqueries.py
@@ -11,26 +11,40 @@ def create_connection():
         print(e)
         return None
 
-def get_questions_by_category():
-    conn = sqlite3.connect(DBFILE)
-    cursor = conn.cursor()
-    cursor.execute('''
-        SELECT q.question, q.answer 
-        FROM questions q
-        JOIN groups g ON g.id_group = q.id_group
-        JOIN categories c ON c.id_category = q.id_category
-    ''',)
-    rows = cursor.fetchall()
-    conn.close()
+def get_questions_en():
+    with sqlite3.connect(DBFILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT q.question, q.answer, g.name, c.name, q.votes 
+            FROM questions q
+            JOIN groups g ON g.id_group = q.id_group
+            JOIN categories c ON c.id_category = q.id_category
+        ''')
+        rows = cursor.fetchall()
     return rows
 
+#try:
+    rows = get_questions_en()
+    for question, answer, group, category, votes in rows:
+        print(question)
+        print(answer)
+        print(group)
+        print(category)
+        print(votes)
+#except Exception as e:
+    print(f"An error occurred: {e}")
 
-create_connection()
-questions = get_questions_by_category()
-print(f'Questions:}')
-for question, answer in questions:
-    print(f'Q: {question}')
-    print(f'A: {answer}')
-    print()
+def get_questions_category_en(category_id):
+    with sqlite3.connect(DBFILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT q.question, q.answer, g.name, c.name, q.votes 
+            FROM questions q
+            JOIN groups g ON g.id_group = q.id_group
+            JOIN categories c ON c.id_category = q.id_category
+            WHERE c.id_category = ?
+        ''', (category_id,))
+        rows = cursor.fetchall()
+    return rows
 
-    
+#input("Press Enter to exit...")
